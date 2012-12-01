0cd8cdc918f89ccda217fb85f6314290fa597afd
Peter Kropf <peter@thecrucible.org>
2012-11-30 18:35:01 -0800

    fixed cleanup of names before matching.

--- tutorials.py
+++ tutorials.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-fa = [r.strip() for r in open('fa_names.txt.private', 'r').readlines()]
-sp = [r.strip() for r in open('tutorial_names.txt.private', 'r').readlines()]
+fa = [' '.join(r.strip().split()).lower() for r in open('fa_names.txt.private', 'r').readlines()]
+sp = [' '.join(r.strip().split()).lower() for r in open('tutorial_names.txt.private', 'r').readlines()]
 
 for name in sp:
     if name in fa:
